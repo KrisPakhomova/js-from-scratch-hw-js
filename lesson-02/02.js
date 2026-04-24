d841a70811f5bb2dba6567fdbbfb33381cda21e1
KrisPakhomova <dokrisbang@gmail.com>
2026-04-24 16:57:20 +0300

    second lesson

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,16 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 7); // your code
+let dice2 = Math.floor(Math.random() * 7); // your code
+let isWinningDouble = false; // your code
 
-/*
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
+console.log("Первый бросок: " + dice1);
+console.log("Второй бросок: " + dice2);
+
+if (dice1 === dice2 && dice1 > 3) {
+  console.log("Выигрышный дубль!");
+  isWinningDouble = true;
 } else {
-  console.log('Не выигрышный дубль.')
+  console.log("Не выигрышный дубль.");
 }
-*/

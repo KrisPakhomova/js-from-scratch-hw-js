dc32d678ebe0a75846674328def2e3919c44397a
KrisPakhomova <dokrisbang@gmail.com>
2026-06-19 17:19:00 +0300

    switch img

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,20 +17,21 @@ const imgElement = document.querySelector("#web-tech-image");
 
 let currentImgIndex = 0;
 
-// Устанавливаем первую картинку
-imgElement.src = WEB_TECH_IMAGES[currentImgIndex];
-
 const WEB_TECH_IMAGES = [
   "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg",
   "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg",
   "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg",
 ];
 
+// Устанавливаем первую картинку
+imgElement.src = WEB_TECH_IMAGES[currentImgIndex];
+
 prevButton.addEventListener("click", function () {
   currentImgIndex--;
   if (currentImgIndex < 0) {
     currentImgIndex = WEB_TECH_IMAGES.length - 1;
   }
+  imgElement.src = WEB_TECH_IMAGES[currentImgIndex];
 });
 
 nextButton.addEventListener("click", function () {
@@ -38,4 +39,5 @@ nextButton.addEventListener("click", function () {
   if (currentImgIndex == WEB_TECH_IMAGES.length) {
     currentImgIndex = 0;
   }
+  imgElement.src = WEB_TECH_IMAGES[currentImgIndex];
 });

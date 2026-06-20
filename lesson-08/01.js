7a81c6cbe4040ab8548b01a6030fcbccbe079cfe
KrisPakhomova <dokrisbang@gmail.com>
2026-06-20 17:44:26 +0300

    capitalize words

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -5,5 +5,28 @@
   🧙‍♂️Эту задачу можно решить при помощь цикла for. Рекомендуем реализовать этот вариант решения, как самый эффективный.
   Другой вариант - использовать метод split строк, и метод массива join.
 */
+function capitalizeWords(str) {
+  let strUpperCase = "";
+  strUpperCase += str.charAt(0).toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str.charAt(i - 1) === " ") {
+      strUpperCase += str.charAt(i).toUpperCase();
+    } else {
+      strUpperCase += str.charAt(i);
+    }
+  }
+  return strUpperCase;
+}
 
-function capitalizeWords() {}
+// function capitalizeWords(str) {
+//   const words = str.split(' '); // разбиваем на слова
+//   let result = [];
+
+//   for (let i = 0; i < words.length; i++) {
+//     const word = words[i];
+//     const capitalized = word[0].toUpperCase() + word.slice(1);
+//     result.push(capitalized);
+//   }
+
+//   return result.join(' ');
+// }
